b34652924e50a95ed66cd4902c39ca1279f4535e
aaron <aaronbwillis@msn.com>
2018-09-04 21:42:49 -0600

    Initial commit

---
+++ app/src/main/java/project/udacity/awillis/singlescreen_aw/MainActivity.java
@@ -0,0 +1,13 @@
+package project.udacity.awillis.singlescreen_aw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
